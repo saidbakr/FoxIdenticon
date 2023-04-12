68ddab171d334a8cee4bb39f33571b5e7db50c39
Said Bakr <said.fox@gmail.com>
2023-04-12 21:07:43 +0200

    Improve parameters requesting in avatar.php

--- avatar.php
+++ avatar.php
@@ -2,8 +2,10 @@
 namespace saidbakr;
 require_once('FoxIdenticon.php');
 use \saidbakr\FoxIdenticon;
-FoxIdenticon::$salt = 'S0meS@1tString'; // Optional to keep unique output for each app.
-FoxIdenticon::create($_GET['str']??null,$_GET['s']??null,true,false);//Generate trimmed border and thin
+$str = $_GET['str']??null;
+$size = (empty($_GET['s']) || !is_numeric($_GET['s']))? null : $_GET['s'];
+//FoxIdenticon::$salt = 'S0meS@1tString'; // Optional to keep unique output for each app.
+FoxIdenticon::create($str,$size,true,false);//Generate trimmed border and thin
 
 //FoxIdenticon::create($_GET['str']??null,$_GET['s']??null,true,true);//Generate trimmed border and thick
 
